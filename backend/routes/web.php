19da0a55fa1e78def0e28b4f9d38c3c6ddd7e3c8
rattsl <r4ttsl@gmail.com>
2021-08-27 21:07:37 +0900

    feat: increment hello route

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('hello', function(){
+    return "<html><body><h1>hello, world</h1></body></html>";
+});
