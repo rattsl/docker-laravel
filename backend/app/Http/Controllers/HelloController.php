b853cc92836f0694fadb2c4268652adb874d2232
rattsl <r4ttsl@gmail.com>
2021-08-29 19:53:42 +0900

    feat: bladeを使ってクライアントに変数を渡す処理を実装

--- backend/app/Http/Controllers/HelloController.php
+++ backend/app/Http/Controllers/HelloController.php
@@ -7,27 +7,8 @@ use Illuminate\Http\Request;
 class HelloController extends Controller
 {
     //アクションメソッドの追記
-    public function index($id="noname", $pass="nopass") {
-        return <<<EOF
-        <html>
-            <head>
-                <title>Hello/index</title>
-                <style>
-                h1{
-                    text-align: center;
-                    color: #eee;
-                }
-                </style>
-            </head>
-            <body>
-                <h1>INDEX</h1>
-                <p>これはhelloコントローラーのインデックスメソッドです</p>
-                <ul>
-                    <li>ID: {$id}</li>
-                    <li>Pass: {$pass}</li>
-                </ul>
-            </body>
-        </html>
-        EOF;
+    public function index() {
+        $data = ['msg' => 'これはbladeを利用したmessageです'];
+        return view( 'index', $data );
     }
 }
---
+++ backend/resources/views/index.blade.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+    <title>hello/index</title>
+    <style>
+        body{
+            font-size: 16px;
+            color: #999;
+        }
+        h1{
+            font-size: 50px;
+            text-align: right;
+            color: #f6f6f6;
+        }
+    </style>
+</head>
+<body>
+    <h1>Blade/index</h1>
+    <p>{{ $msg }}</p>
+</body>
+</html>
