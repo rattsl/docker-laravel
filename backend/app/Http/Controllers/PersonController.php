ee07e6d4109de0bf33fb5179501c1c890d01b42e
rattsl <r4ttsl@gmail.com>
2021-09-05 18:15:13 +0900

    mod: Personコントローラの変更

--- backend/app/Http/Controllers/PersonController.php
+++ backend/app/Http/Controllers/PersonController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Person;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redis;
 
 class PersonController extends Controller
 {
-    //
+   public function index (Request $request){
+    $items = Person::all();
+    return view('person.index', [ 'items' => $items ]);
+   }
 }
